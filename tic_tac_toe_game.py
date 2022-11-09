11b0c2374f680905683d7a142ac012295eb486c0
FlorianGrollich <F-Grollich@web.de>
2022-11-09 19:47:26 +0100

    feat: implemented next move

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -9,21 +9,27 @@ class Player(Enum):
 
 class TicTacToeGame:
     def __init__(self):
-        # "#" = empty
-        self.game_field: list[list[Player]] = [
-            [Player.N, Player.N, Player.N],
-            [Player.N, Player.N, Player.N],
-            [Player.N, Player.N, Player.N],
-        ]
-        self.round: int = 0
+        #   INDEX for playing field
+        #   0 | 1 | 2
+        #   ---------
+        #   3 | 4 | 5
+        #   ---------
+        #   6 | 7 | 8
+        self.game_field: list[Player] = [
+            Player.N, Player.N, Player.N,
+            Player.N, Player.N, Player.N,
+            Player.N, Player.N, Player.N]
 
-    #   INDEX for playing field
-    #   0 | 1 | 2
-    #   ---------
-    #   3 | 4 | 5
-    #   ---------
-    #   6 | 7 | 8
+        self.round: int = 1
+
     def next_move(self, index: int) -> None:
-        pass
+        next_player = Player.X if self.round % 2 == 0 else Player.O
+
+        if self.game_field[index] == Player.N:
+            self.game_field[index] = next_player
+            self.round += 1
+        else:
+            raise Exception("There is already a player on that field")
+
     def check_for_win(self) -> bool:
         pass
