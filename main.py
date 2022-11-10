c7f4292235a0239a3d7c46f892217adcb64bfc56
FlorianGrollich <F-Grollich@web.de>
2022-11-10 11:04:18 +0100

    feat: add make next move and next_move just return game state

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from tic_tac_toe_game import TicTacToeGame
+
+game = TicTacToeGame()
+
+game.make_next_move(1)
+game.print_game_field()
--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -15,7 +15,7 @@ class TicTacToeGame:
         #   3 | 4 | 5
         #   ---------
         #   6 | 7 | 8
-        self.game_field: list[FieldState] = [
+        self._game_field: list[FieldState] = [
             FieldState.N, FieldState.N, FieldState.N,
             FieldState.N, FieldState.N, FieldState.N,
             FieldState.N, FieldState.N, FieldState.N]
@@ -24,25 +24,30 @@ class TicTacToeGame:
 
     def print_game_field(self):
         print(f"""
-        {self.game_field[0].value} | {self.game_field[1].value} | {self.game_field[2].value}
+        {self._game_field[0].value} | {self._game_field[1].value} | {self._game_field[2].value}
         ---------
-        {self.game_field[3].value} | {self.game_field[4].value} | {self.game_field[5].value}
+        {self._game_field[3].value} | {self._game_field[4].value} | {self._game_field[5].value}
         --------- 
-        {self.game_field[6].value} | {self.game_field[7].value} | {self.game_field[8].value}
+        {self._game_field[6].value} | {self._game_field[7].value} | {self._game_field[8].value}
         """)
 
-    def next_move(self, index: int) -> None:
+    def next_move(self, index: int) -> list[FieldState]:
         next_player = FieldState.X if self.round % 2 == 0 else FieldState.O
 
-        if self.game_field[index] == FieldState.N:
-            self.game_field[index] = next_player
-            self.round += 1
+        if self._game_field[index] == FieldState.N:
+            field = self._game_field.copy()
+            field[index] = next_player
+            return field
         else:
             raise Exception("There is already a player on that field")
 
+    def make_next_move(self, move: int):
+        self._game_field = self.next_move(move)
+        self.round += 1
+
     def get_possible_moves(self) -> list[int]:
         result = []
-        for i, field in enumerate(self.game_field):
+        for i, field in enumerate(self._game_field):
             if field == FieldState.N:
                 result.append(i)
         return result
@@ -59,12 +64,10 @@ class TicTacToeGame:
             [2, 4, 6],
         ]
         for pos in win_possibilities:
-            is_win = len({self.game_field[pos[0]], self.game_field[pos[1]], self.game_field[pos[2]]}) == 1
+            is_win = len({self._game_field[pos[0]], self._game_field[pos[1]], self._game_field[pos[2]]}) == 1
             if is_win:
-                if self.game_field[pos[0]] == FieldState.X:
+                if self._game_field[pos[0]] == FieldState.X:
                     return 1
-                elif self.game_field[pos[0]] == FieldState.O:
+                elif self._game_field[pos[0]] == FieldState.O:
                     return -1
         return 0
-
-
